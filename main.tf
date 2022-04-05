6092a1fced3fc90d4b6f645883476facb9e08dd2
Nick Ruter <nickruter@gmail.com>
2022-04-05 09:55:52 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,16 @@ terraform {
     }
   }
 }
+   terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
 
+  required_version = ">= 1.1.0"
+}
 provider "aws" {
   version = "~> 3.0"
   region  = "us-east-1"
