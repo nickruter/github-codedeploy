c3e64df4a426e069a07e7a957856f1f02b0a8e7f
Nick Ruter <nruter@gannett.com>
2022-04-04 23:05:46 -0400

    update

--- main.tf
+++ main.tf
@@ -1,29 +1,19 @@
- # The configuration for the `remote` backend.
-     terraform {
-       backend "remote" {
-         # The name of your Terraform Cloud organization .
-         organization = "Nickel"
+# The configuration for the `remote` backend.
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization .
+    organization = "Nickel"
 
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "workspace-nickel"
-         }
-       }
-     }
-
-     terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "workspace-nickel"
     }
   }
-
-  required_version = ">= 1.1.0"
 }
 
 provider "aws" {
-  region  = "us-east-2"
+  version = "~> 3.0"
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "app_server" {
