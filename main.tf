bacc7153acf7061f77fc9f3a32b6329d1e9f3002
Nick Ruter <nickruter@gmail.com>
2022-04-05 10:30:21 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,6 +24,8 @@
 
 provider "aws" {
   region  = "us-east-2"
+    access_key = "secrets.AWS_ACCESS_KEY_ID"
+    secret_key = "secrets.AWS_SECRET_ACCESS_KEY"
 }
 
 resource "aws_instance" "app_server" {
