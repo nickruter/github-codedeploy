d5ecdfe716402c10bdfcf6879de75dc71e56ad68
nickruter <79154545+nickruter@users.noreply.github.com>
2022-04-05 10:52:42 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,8 +24,8 @@
 
 provider "aws" {
   region  = "us-east-2"
-    access_key = "secrets.AWS_ACCESS_KEY_ID"
-    secret_key = "secrets.AWS_SECRET_ACCESS_KEY"
+    access_key = "{{secrets.AWS_ACCESS_KEY_ID}}"
+    secret_key = "{{secrets.AWS_SECRET_ACCESS_KEY}}"
 }
 
 resource "aws_instance" "app_server" {
